acdc1a6940bb17f6855906735dc07bbb51282575
jjackson2621 <162258305+jjackson2621@users.noreply.github.com>
2024-04-01 21:45:49 -0500

    custom command

---
+++ customcommands.js
@@ -0,0 +1,7 @@
+Cypress.Commands.add('logintoSauceDemo', (username, password)=> {
+    cy.visit('https://www.saucedmo.com/');
+    cy.get('#user-name').type(username);
+    cy.get('#password').type(password);
+    cy.get('#login-button').click();
+
+});
